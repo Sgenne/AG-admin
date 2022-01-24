f9e8777ffe4f7aee018b436af717ea70b0803373
Sgenne <simon.genne@gmail.com>
2022-01-24 21:37:29 +0100

    Added blog post list item

--- src/components/blog/PostList.tsx
+++ src/components/blog/PostList.tsx
@@ -1,12 +1,19 @@
 import "../../css/blog/PostList.css";
 import { IBlogPost } from "../../interfaces/blog";
+import PostListItem from "./PostListItem";
 
 interface IPostListProps {
   posts: IBlogPost[];
 }
 
 const PostList = ({ posts }: IPostListProps) => {
-  return <div></div>;
+  const listItems = posts.map((post) => (
+    <li className="blog-post-list__item-container" key={post._id}>
+      <PostListItem post={post} />
+    </li>
+  ));
+
+  return <ul className="blog-post-list">{listItems}</ul>;
 };
 
 export default PostList;
--- src/interfaces/blog.ts
+++ src/interfaces/blog.ts
@@ -1,5 +1,6 @@
 export interface IBlogPost {
-    title: string;
-    content: string;
-    createdAt: string;
+  _id: string;
+  title: string;
+  content: string;
+  createdAt: string;
 }
